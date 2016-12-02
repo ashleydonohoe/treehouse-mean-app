177b9d47468913211940c11da89618eac1a3b436
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-12-02 00:11:39 -0500

    added put, get and post routes to API

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@
 
 var express = require("express"),
     app     = express(),
-    router  = require("./api");
+    router  = require("./api"),
+    parser  = require("body-parser");
 
 require('./database');
+require('./seed');
 
 app.use(express.static('public'));
+app.use(parser.json());
 app.use("/api", router);
+
 
 app.listen(3000, function(){
     console.log("Server is running!");
